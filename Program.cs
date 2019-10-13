71117a4f4d1034862cb1931cd50acccb1adabb1e
kebo <bogdan.parubok@hotmail.com>
2019-10-13 17:41:46 +0300

    added loop and exit path

--- Program.cs
+++ Program.cs
@@ -10,25 +10,40 @@ namespace tempCalc
             float temp;
             double celsius;
             double farenheit;
+            bool loop = false;
 
             Console.WriteLine("Hello World!");
             Console.WriteLine("This is Temperature Calculator. This calculator lets you calculate Farenheith into Celsius and vice-versa.");
-            Console.WriteLine("Insert \"FC\" if you cant to calculate Farenheit into Celsius or"); 
-            Console.WriteLine("insert \"CF\" if you want to calculate Celsius into Farenheit.");
-            userChoice = Console.ReadLine();
-            Console.WriteLine("Please insert temperature value you want to convert.");
-            temp = float.Parse(Console.ReadLine());
 
-            if (userChoice.ToUpper() == "FC")
+            while (!loop)
             {
-                celsius = (temp - 32) * 1.8;
-                Console.WriteLine($"The {temp}F degrees is {celsius}C degrees.");
-            }
+                Console.WriteLine("Insert \"FC\" if you cant to calculate Farenheit into Celsius or");
+                Console.WriteLine("insert \"CF\" if you want to calculate Celsius into Farenheit.");
+                Console.WriteLine("Insert any other button if you want to exit calculator.");
+                userChoice = Console.ReadLine();
+                Console.WriteLine("\n");
 
-            else
-            {
-                farenheit = temp * 1.8 + 32;
-                Console.WriteLine($"The {temp}C degrees is {farenheit}F degrees.");
+                if (userChoice.ToUpper() == "FC")
+                {
+                    Console.WriteLine("Please insert F temperature value you want to convert.");
+                    temp = float.Parse(Console.ReadLine());
+                    celsius = (temp - 32) * 1.8;
+                    Console.WriteLine($"The {temp}F degrees is {celsius}C degrees.");
+                }
+
+                else if (userChoice.ToUpper() == "CF")
+                {
+                    Console.WriteLine("Please insert C temperature value you want to convert.");
+                    temp = float.Parse(Console.ReadLine());
+                    farenheit = temp * 1.8 + 32;
+                    Console.WriteLine($"The {temp}C degrees is {farenheit}F degrees.");
+                }
+                
+                else
+                {
+                    loop = true;
+                }
+                Console.WriteLine("\n");
             }
         }
     }
